f9cdaf572ece813da01d493ba5717228b5f97696
banggus888-hub <banggus888@gmail.com>
2026-09-06 21:19:26 +0700

    banggusss

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Go = require('@xof/fetch');
 const { initializeApp } = require('firebase/app');
 const { getDatabase, ref, get, set, child } = require('firebase/database');
 
+const config = {
   base: 'https://restapidhan.vercel.app',
   apikey: 'freeapikeydhan26'
 };
